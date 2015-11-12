c8b2e940aa8a228e450c5e1fcb2783eefa42f964
Stephen Levenhagen <slevenhagen@novapointgroup.com>
2015-11-12 10:33:56 -0500

    Update kanban and list views for Days Old Field Fixed issue with correct Form and list veiws for lead or Opportunity on Menu window actions

--- addons-extra/genemedics/crm_genemedics/crm_lead.py
+++ addons-extra/genemedics/crm_genemedics/crm_lead.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import api, fields, models, _     
+from openerp import api, fields, models, _ 
+from datetime import datetime, timedelta  
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT 
 from __builtin__ import False
 
 class crm_lead(models.Model):
     """ CRM Lead Case """
     _inherit = "crm.lead"
 
-    
+    @api.one
+    def _cal_age(self):
+        
+        delta = datetime.now() - datetime.strptime(self.create_date, DEFAULT_SERVER_DATETIME_FORMAT)
+        self.age_days = int(delta.days)
+   
     customer_goal = fields.Char('Customer Goal', size = 32) 
+    age_days = fields.Integer(string = "Days Old",compute="_cal_age",store=False)
+    
+    _order = 'create_date'
     
     @api.multi
     def action_schedule_meeting(self):
@@ -26,13 +36,13 @@ class crm_lead(models.Model):
         return res
     
     @api.multi
-    def action_schedule_meeting_form(self):
+    def action_schedule_meeting_lead(self):
         """
         Open meeting's calendar view to schedule meeting on current opportunity.
         :return dict: dictionary value for created Meeting view
         """
         IrModelData = self.env['ir.model.data']
-        res = self.env['ir.actions.act_window'].for_xml_id('crm_genemedics', 'action_calendar_event_form')
+        res = self.env['ir.actions.act_window'].for_xml_id('crm_genemedics', 'action_calendar_event_calendar')
         lead = self
         
         partner_ids = [self.user_id.partner_id.id]
