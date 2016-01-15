708a360336cac15b36e224b60282874e01029c32
Stephen Levenhagen <slevenhagen@novapointgroup.com>
2016-01-15 15:39:16 -0500

    UPdate Search Filter methods On Issue A Issue Age

--- addons-extra/genemedics/crm_helpdesk_features/models/project_issue.py
+++ addons-extra/genemedics/crm_helpdesk_features/models/project_issue.py
@@ -26,21 +26,23 @@ class project_issue(models.Model):
         
         
          
-        assert operator in ('=', '!=', '<=','>=','<','>') and days , 'Operation not supported' 
+        assert operator in ('=', '!=', '<=','>=','<','>') and value , 'Operation not supported' 
         
-        if operator == '<=': operator ='>='
-        if operator == '>=': operator ='<='
-        if operator == '<': operator ='>'
-        if operator == '>': operator ='<'
-        search_date = datetime.now() - timedelta(hours=value)
+        if operator == '<=': rev_operator ='>='
+        if operator == '>=': rev_operator ='<='
+        if operator == '<': rev_operator ='>'
+        if operator == '>': rev_operator ='<'
+        
+        time_now = datetime.now()
+        search_date =  time_now - timedelta(hours=value)
         search_date = datetime.strftime(search_date,DEFAULT_SERVER_DATETIME_FORMAT)
-        issues = self.env['project.issue'].search([('create_date', operator, search_date)])
-        return [('id', 'in', leads.ids)]
+        issues = self.env['project.issue'].search([('create_date', rev_operator, search_date)])
+        return [('id', 'in', issues.ids)]
         
     @api.one
     def _cal_update_age_issue(self):
         
-        delta = datetime.now() - datetime.strptime(self.date_action_last, DEFAULT_SERVER_DATETIME_FORMAT)
+        delta = datetime.now() - datetime.strptime(self.write_date, DEFAULT_SERVER_DATETIME_FORMAT)
         hours = delta.days * 24.0 + delta.seconds / 3600.0
         self.age_since_update = hours
                
@@ -49,16 +51,16 @@ class project_issue(models.Model):
         
         
          
-        assert operator in ('=', '!=', '<=','>=','<','>') and days , 'Operation not supported' 
+        assert operator in ('=', '!=', '<=','>=','<','>') and value , 'Operation not supported' 
         
-        if operator == '<=': operator ='>='
-        if operator == '>=': operator ='<='
-        if operator == '<': operator ='>'
-        if operator == '>': operator ='<'
+        if operator == '<=': rev_operator ='>='
+        if operator == '>=': rev_operator ='<='
+        if operator == '<': rev_operator ='>'
+        if operator == '>': rev_operator ='<'
         search_date = datetime.now() - timedelta(hours=value)
         search_date = datetime.strftime(search_date,DEFAULT_SERVER_DATETIME_FORMAT)
-        issues = self.env['project.issue'].search([('date_action_last', operator, search_date)])
-        return [('id', 'in', leads.ids)]
+        issues = self.env['project.issue'].search([('write_date', rev_operator, search_date)])
+        return [('id', 'in', issues.ids)]
         
     age_issue = fields.Float(string = "Hours Since Created",compute="_cal_age_issue",store=False, search='_search_age_issue', help="Issue Age in Hours")
-    age_since_update = fields.Float(string = "Hours Since Action",compute="_cal_update_age_issue",store=False, search='_search_update_age_issue', help="Issue Age Since las Update")
+    age_since_update = fields.Float(string = "Hours Since Update",compute="_cal_update_age_issue",store=False, search='_search_update_age_issue', help="Issue Age Since las Update")
