8701332eba47a3073e97bc74f45479672fcb04fd
Naitik Mehta <naitik.mehta@serpentcs.com>
2016-03-08 18:18:26 +0530

    [IMP] Improved Appointment Scheduling Process to change city string.

--- addons-extra/genemedics/appointment_scheduling_management/controllers/main.py
+++ addons-extra/genemedics/appointment_scheduling_management/controllers/main.py
@@ -47,41 +47,34 @@ class website_appointment_scheduling(http.Controller):
 #        }
 #        return data
     
-    @http.route('/search_slot', type='json', method='post', website=True)
+    @http.route('/search_slot', type='json', auth="public", method='post', website=True)
     def search_slot(self, **kw):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        slot_obj = pool['slot.slot']
-        event_obj = pool['calendar.event']
-        tz = context and pytz.timezone(context.get('tz', False))
-        slot_ids = slot_obj.search(cr, uid, [], context=context)
-        avail_slots = []
         slot = []
-        meeting_obj = pool['calendar.event.type']
-        duration = meeting_obj.browse(cr, SUPERUSER_ID, int(kw.get('meeting_type')), context=context).duration
+        slot_obj = pool['slot.slot']
+        slot_ids = slot_obj.search(cr, uid, [], context=context)
+        meeting_rec = pool['calendar.event.type'].browse(cr, SUPERUSER_ID, int(kw.get('meeting_type')), context=context)
         for slot_rec in slot_obj.browse(cr, uid, slot_ids, context=context):
-#            new_sdate = datetime.strptime(kw.get('adate'), '%d/%m/%Y')
-#            new_stdate = datetime.strftime(datetime.strptime(kw.get('adate'), '%d/%m/%Y'), '%Y-%m-%d')
-#            st_date = tz.localize(start_final_date, is_dst=None).astimezone(pytz.utc).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-#            et_date = tz.localize(end_final_date, is_dst=None).astimezone(pytz.utc).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+            employee = False
             st_date = datetime.strptime(datetime.strftime(datetime.strptime(kw.get('adate'), '%d/%m/%Y'), '%Y-%m-%d') + ' ' + slot_rec.sname, '%Y-%m-%d %H:%M:%S')
             start_date = datetime.strftime((st_date + timedelta(seconds=1)), DEFAULT_SERVER_DATETIME_FORMAT)
-#            end_date = datetime.strptime(new_stdate + ' ' + slot_rec.ename, '%Y-%m-%d %H:%M:%S').strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-            end_date = datetime.strftime(st_date + timedelta(hours=duration, seconds=-1), DEFAULT_SERVER_DATETIME_FORMAT)
-#            start_date = tz.localize(start_date, is_dst=None).astimezone(pytz.utc).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-#            end_date = tz.localize(end_date, is_dst=None).astimezone(pytz.utc).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-            cr.execute("select id from calendar_event where "\
-                    "location_id="+ str(int(kw.get('location_id'))) +" and "\
-                    "employee_id="+ str(int(kw.get('employee_id'))) +" and "\
-                    "(((start_datetime between '"+start_date+"' and '"+end_date+"') or (stop_datetime between '"+start_date+"' and '"+end_date+"')) or "\
-                    "(start_datetime = '"+start_date+"' and stop_datetime = '"+end_date+"') or "\
-                    "('"+start_date+"' between start_datetime and stop_datetime))")
-            if len(cr.fetchall()) == 0:
-                slot.append({'avail': False, 'name': slot_rec.sname})
-            else:
-                slot.append({'avail': True, 'name': slot_rec.sname})
+            end_date = datetime.strftime(st_date + timedelta(hours=meeting_rec.duration, seconds=-1), DEFAULT_SERVER_DATETIME_FORMAT)
+            for consultant in meeting_rec.consultant_ids:
+                cr.execute("select id from calendar_event where "\
+#                        "location_id="+ str(int(kw.get('location_id'))) +" and "\
+                        "employee_id="+ str(consultant.employee_id.id) +" and "\
+                        "(((start_datetime between '"+start_date+"' and '"+end_date+"') or (stop_datetime between '"+start_date+"' and '"+end_date+"')) or "\
+                        "(start_datetime = '"+start_date+"' and stop_datetime = '"+end_date+"') or "\
+                        "('"+start_date+"' between start_datetime and stop_datetime))")
+                if not cr.fetchall():
+                    employee = consultant.employee_id.id
+                    break
+            res = {'avail': True, 'name': slot_rec.sname, 'employee': employee}
+            if employee:
+                res.update({'avail': False})
+            slot.append(res)
         return request.website._render('appointment_scheduling_management.display_time_slot', {'slot': slot})
-    
-    
+
     @http.route('/meeting_type_onchange', type='json', method='post', website=True)
     def meeting_type_onchange(self, **kw):
         emp = []
@@ -96,24 +89,24 @@ class website_appointment_scheduling(http.Controller):
             'list_employee_ids': vals.get('value').get('employee_ids')[0][2]
         }
 
-    @http.route('/meeting_type', type='json', method='post', website=True)
+    @http.route('/meeting_type', type='json', auth="public", method='post', website=True)
     def meeting_type(self, **kwargs):
         data = []
         domain = []
         state_id = int(kwargs.get('state_id'))
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         meeting_obj = pool['calendar.event.type']
-        loc_ids = pool['res.country.state'].search(cr, SUPERUSER_ID, [('code', '=', 'FL')], limit=1, context=context)
-        if state_id != loc_ids[0]:
-            domain.append('|')
+        loc_ids = pool['res.country.state'].search(cr, SUPERUSER_ID, ['|', ('code', '=', 'MI'), '|', ('code', '=', 'AZ'), ('code', '=', 'FL')], context=context)
+        if state_id not in loc_ids:
             domain.append(('state_id', '=', False))
-        domain.append(('state_id', '=', state_id))
+        else:
+            domain.append(('state_id', '=', state_id))
         type_ids = meeting_obj.search(cr, SUPERUSER_ID, domain, context=context)
         for rec in meeting_obj.browse(cr, SUPERUSER_ID, type_ids, context=context):
-            data.append([rec.id, rec.name])
+            data.append([rec.id, rec.name, rec.loc])
         return data
 
-    @http.route('/employees', type='json', method='post', website=True)
+    @http.route('/employees', type='json', auth="public", method='post', website=True)
     def employees(self, **kwargs):
         data = []
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
@@ -122,12 +115,11 @@ class website_appointment_scheduling(http.Controller):
             data.append([rec.id, rec.name])
         return data
 
-    @http.route('/create_appointment', type='json', method='post', website=True)
+    @http.route('/create_appointment', type='json', auth="public", method='post', website=True)
     def create_appointment(self, **kwargs):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         event_obj = request.registry['calendar.event']
         event_type_obj = request.registry['calendar.event.type']
-        tz = context and pytz.timezone(context.get('tz', False))
         start_datetime = datetime.strptime(kwargs.get('date') + ' ' + kwargs.get('time_slot'), '%d/%m/%Y %H:%M:%S').strftime(DEFAULT_SERVER_DATETIME_FORMAT)
         duration = event_type_obj.browse(cr, SUPERUSER_ID, int(kwargs.get('meeting_type')), context=context).duration
         vals = {
@@ -136,28 +128,38 @@ class website_appointment_scheduling(http.Controller):
             'location_id': int(kwargs.get('location_id')),
             'meeting_type': int(kwargs.get('meeting_type')),
             'start_datetime': start_datetime,
+            'state': 'open',
             'duration': duration,
         }
         onchange_vals = event_obj.onchange_duration(cr, SUPERUSER_ID, [], start_datetime, duration, context=context)
         vals.update(onchange_vals.get('value'))
-        event_obj.create(cr, SUPERUSER_ID, vals, context=context)
+        event_id = event_obj.create(cr, SUPERUSER_ID, vals, context=context)
+        
+        template_id = pool['ir.model.data'].get_object_reference(cr, uid, 'appointment_scheduling_management', 'consultant_notification_template')[1]
+        msg_id = pool['mail.template'].send_mail(cr, uid, template_id, event_id, force_send=False, context=context)
+        if msg_id:
+            pool['mail.mail'].send(cr, uid, [msg_id], context=context)
         return True
 
-    @http.route('/check_user_group', type='json', method='post', website=True)
+    @http.route('/check_user_group', type='json', auth="public", method='post', website=True)
     def check_user_group(self, **kwargs):
         return request.registry['res.users'].has_group(request.cr, request.uid, 'appointment_scheduling_management.lead_user')
 
 
     @http.route('/appointment', type='http', auth="public", website=True, csrf=False)
     def appointment(self, **kwargs):
+#        if not kwargs:
+#            return request.website.render("appointment_scheduling_management.register")
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         event_obj = request.registry['calendar.event']
         slot_obj = request.registry['slot.slot']
         location_obj = pool['res.country.state']
+        res_country_obj = pool['res.country']
         employee_obj = pool['hr.employee']
         meeting_obj = pool['calendar.event.type']
+        res_country_ids = res_country_obj.search(cr, SUPERUSER_ID, [('code', '=', 'US')], limit=1,context=context)
         emp_ids = employee_obj.search(cr, SUPERUSER_ID, [], context=context)
-        loc_ids = location_obj.search(cr, SUPERUSER_ID, ['|', ('code', '=', 'MI'), '|', ('code', '=', 'AZ'), ('code', '=', 'FL')], context=context)
+        loc_ids = location_obj.search(cr, SUPERUSER_ID, [('country_id', '=', res_country_ids[0])], context=context)
         type_ids = meeting_obj.search(cr, SUPERUSER_ID, [], context=context)
         data = {
             'name': employee_obj.browse(cr, SUPERUSER_ID, emp_ids, context=context),
--- addons-extra/genemedics/appointment_scheduling_management/static/src/js/website_common.js
+++ addons-extra/genemedics/appointment_scheduling_management/static/src/js/website_common.js
@@ -2,7 +2,7 @@ odoo.define('appointment_scheduling_management.appointment_scheduling_management
 var odoo = require('web.ajax');
 
     $(document).ready(function() {
-
+        var employee = false;
         if (window.location.pathname == '/appointment'){
             odoo.jsonRpc('/check_user_group', 'call', {}).then(function (data) {
                 if(data){
@@ -19,24 +19,24 @@ var odoo = require('web.ajax');
             dateFormat : 'dd/mm/yy',
         });
 
-        $(document).on('change', '#id_start_date,#employee_id,select[name="meeting_type"],select[name="location_id"]', function(){
+        $(document).on('change', '#id_start_date,select[name="meeting_type"]', function(){
             var view = {
                 'adate': $('#id_start_date').val(),
-                'employee_id': $('#employee_id').val(),
                 'meeting_type': $('select[name="meeting_type"]').val(),
                 'location_id': $('select[name="location_id"]').val()
             }
-            if(view['adate'] && view['employee_id'] && view['meeting_type'] && view['location_id']){
+            if(view['adate'] && view['meeting_type'] && view['location_id']){
                 odoo.jsonRpc('/search_slot', 'call', view).then(function (data) {
-                	$('.time-slots').fadeOut("slow", function() {
-                	    $(this).html(data).fadeIn("medium");
+                    $('.time-slots').fadeOut("slow", function() {
+                        $(this).html(data).fadeIn("medium");
+                        $('#slot_select').val('')
+                        employee = false;
                     });
                 });
             }
         });
 
         $(document).on('change', 'select[name="location_id"]', function() {
-            $('#city-label').text($('select[name="location_id"] option:selected').data('city'));
             var select = $('select[name="meeting_type"]');
             if ($(this).val()){
                 odoo.jsonRpc('/meeting_type', 'call', {'state_id': $(this).val()}).then(function (data) {
@@ -46,50 +46,59 @@ var odoo = require('web.ajax');
 //                  select.append($(option));
                   $.each(data, function(key,value) {
                       option = new Option(value[1],value[0]);
+                      $(option).attr( "data-location", value[2] );
                       select.append($(option));
                   });
               }).then(function(){
                   select.change();
               })
             }
-            
         });
-        
-        $(document).on('change', 'select[name="meeting_type"]', function() {
-            var select = $('select[name="employee_id"]');
-            if($(this).val()){
-                odoo.jsonRpc('/employees', 'call', {'meeting_type': $(this).val()}).then(function (data) {
-                  $('option', select).remove();
-//                  $(option).attr("disabled", "disabled");
-//                  $(option).attr("selected", "selected");
-//                  select.append($(option));
-                  $.each(data, function(key,value) {
-                      option = new Option(value[1],value[0]);
-                      select.append($(option));
-                  });
-              }).then(function(){
-                  select.change();
-              })
-            }
-        })
-        
+
+//        $(document).on('change', 'select[name="meeting_type"]', function() {
+//            if($('select[name="meeting_type"] option:selected').data('location') == 'physical_loc'){
+//                $('#city-label').text($('select[name="location_id"] option:selected').data('city'));
+//            }else{
+//                $('#city-label').text('');
+//            }
+//        })
+
         $(document).on('click', '.time button', function() {
             if ($(this).hasClass('btn btn-success')){
                 $('#slot_select').val($(this).text())
+                employee = $(this).data('employee')
             }
         });
 
         $('#submit_botton').click(function(){
-            odoo.jsonRpc('/create_appointment', 'call', {
-                'meeting_type': $('select[name="meeting_type"]').val(),
-                'location_id': $('select[name="location_id"]').val(),
-                'employee_id': $('#employee_id').val(),
-                'date': $('#id_start_date').val(),
-                'time_slot': $('#slot_select').val()
-                }).then(function (data) {
-                    alert('Appointment has been successfully scheduled.');
-                    document.location.reload(true);
-            })
+            if (employee){
+                odoo.jsonRpc('/create_appointment', 'call', {
+                    'meeting_type': $('select[name="meeting_type"]').val(),
+                    'location_id': $('select[name="location_id"]').val(),
+                    'employee_id': employee,
+                    'date': $('#id_start_date').val(),
+                    'time_slot': $('#slot_select').val()
+                    }).then(function (data) {
+                        swal({
+                            title: "Success!",
+                            text: "Appointment has been successfully scheduled.",
+                            type: "success",
+                            confirmButtonClass: 'btn-success',
+                            confirmButtonText: 'Okay!'
+                          },function(isConfirm){
+                              if (isConfirm){
+                                  document.location.reload(true);
+                              }
+                          });
+                })
+            }else{
+                swal({
+                    title: "Oops! You have not selected anything.",
+                    type: "error",
+                    confirmButtonClass: 'btn-danger',
+                    confirmButtonText: 'Cancel'
+                  });
+            }
         })
 
 //        $(document).on('change', 'select[name="meeting_type"]', function() {
